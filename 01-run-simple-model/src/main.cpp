b94d9471daa1ea940295032cdd39558bcd542f32
Dave Bort <dbort@meta.com>
2024-02-16 16:16:27 -0800

    01-run-simple-model: Add SimpleModel.pte; get program path from args

--- 01-run-simple-model/src/main.cpp
+++ 01-run-simple-model/src/main.cpp
@@ -12,25 +12,33 @@
 
 using namespace ::torch::executor;
 
-const std::string ModelFile = "SimpleModel.pte";
+const char* kDefaultProgramPath = "SimpleModel.pte";
 
-int main() {
+int main(int argc, const char** argv) {
+  const char* program_path = kDefaultProgramPath;
+  if (argc == 2) {
+    program_path = argv[1];
+  } else if (argc > 2) {
+    fprintf(stderr, "Usage: %s [program_file]\n", argv[0]);
+    return -1;
+  }
+
   // Load the model .pte file created by the export process. The Module type
   // encapsulates the loaded model.
-  printf("Loading executorch program...\n");
-  Module module(ModelFile.c_str());
+  printf("Loading ExecuTorch program from %s...\n", program_path);
+  Module module(program_path);
 
-  // Set up the model input data. This model takes a 1-dimensions tensor with
+  // Set up the model input data. This model takes a 1-dimensional tensor with
   // four float32 values.
-  float input_values[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-  int32_t sizes[] = {4};
+  float input_values[] = {1.0f, 1.0f, 1.0f, 1.0f};
+  TensorImpl::SizesType sizes[] = {std::size(input_values)};
   TensorImpl input(ScalarType::Float, std::size(sizes), sizes, input_values);
 
   // Run the model.
   auto result = module.forward({EValue(Tensor(&input))});
-
   if (!result.ok()) {
-    printf("Inference error: %i\n", result.error());
+    printf("Inference error: 0x%x\n", (unsigned int)result.error());
+    return -1;
   }
 
   // Retrieve and print the output tensor.

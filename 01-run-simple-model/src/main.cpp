7dd1f7ca347c365ff72824b56534ea910af7d9bc
Dave Bort <dbort@meta.com>
2024-02-16 16:01:31 -0800

    Set up lintrunner and fix lint issues

--- 01-run-simple-model/src/main.cpp
+++ 01-run-simple-model/src/main.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #include <executorch/extension/module/module.h>
 
 #include <cstdio>
@@ -10,26 +18,26 @@ int main() {
   // Load the model .pte file created by the export process. The Module type
   // encapsulates the loaded model.
   printf("Loading executorch program...\n");
-    Module module(ModelFile.c_str());
-    
+  Module module(ModelFile.c_str());
+
   // Set up the model input data. This model takes a 1-dimensions tensor with
   // four float32 values.
-    float input_values[4] = { 1.0f, 1.0f, 1.0f,1.0f };
-    int32_t sizes[] = {4};
-    TensorImpl input(ScalarType::Float, std::size(sizes), sizes, input_values);
+  float input_values[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+  int32_t sizes[] = {4};
+  TensorImpl input(ScalarType::Float, std::size(sizes), sizes, input_values);
 
-   // Run the model.
-    auto result = module.forward({EValue(Tensor(&input))});
+  // Run the model.
+  auto result = module.forward({EValue(Tensor(&input))});
 
-    if (!result.ok()) {
-        printf("Inference error: %i\n", result.error());
-    }
+  if (!result.ok()) {
+    printf("Inference error: %i\n", result.error());
+  }
 
-   // Retrieve and print the output tensor.
-    printf("Output: ");
-    auto output = (*result)[0].toTensor();
-    for (auto i = 0; i < output.numel(); i++) {
-        printf("%f ", output.const_data_ptr<float>()[i]);
-    }
-    printf("\n");
+  // Retrieve and print the output tensor.
+  printf("Output: ");
+  auto output = (*result)[0].toTensor();
+  for (auto i = 0; i < output.numel(); i++) {
+    printf("%f ", output.const_data_ptr<float>()[i]);
+  }
+  printf("\n");
 }

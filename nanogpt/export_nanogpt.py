0db9418440ccf74ddb8e632fd23a948f0c9d81cc
Jack-Khuu <jackkhuu@fb.com>
2024-03-04 13:54:45 -0800

    Updating Export configs, works pending emitter/tensor changes

--- nanogpt/export_nanogpt.py
+++ nanogpt/export_nanogpt.py
@@ -9,39 +9,34 @@ from executorch.exir import to_edge
 import torch
 from torch.export import export
 
+from torch.nn.attention import SDPBackend
 from model import GPT, GPTConfig
+from torch._export import capture_pre_autograd_graph
+from executorch.exir import EdgeCompileConfig, EdgeProgramManager, ExecutorchProgramManager
 
 OUTPUT_FILE = "nanogpt.pte" # Output ExecuTorch program file.
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Prep ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Default Config
-config = GPTConfig()
-
-# GPTConfig from train.py - scratch Input when printing (64, 256)
-config_train = GPTConfig(block_size=256, vocab_size=65, n_layer=6, n_head=6, n_embd=384, dropout=0.2, bias=False)
-
-model = GPT(config)
-
-"""
-for layer in model.children():
-    print(layer)
-    print(type(layer))
-    for name, layer in layer.items():
-        print(name, layer)
-"""
-
-example_inputs = (torch.ones(50304, 768, dtype=torch.int8), )
+model = GPT(GPTConfig())
+example_inputs = (torch.randint(0, 100, (1, 1024), dtype=torch.long), )
 
 #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Export  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Export the pytorch model and process for ExecuTorch.
-print("Exporting program...")
-exported_program = export(model, example_inputs)
-print("Lowering to edge...")
-edge_program = to_edge(exported_program)
-print("Creating ExecuTorch program...")
-et_program = edge_program.to_executorch()
+# Using a custom SDPA kernel for LLMs
+with torch.nn.attention.sdpa_kernel([SDPBackend.MATH]), torch.no_grad():
+    m = capture_pre_autograd_graph( model, example_inputs)
+    edge_config = EdgeCompileConfig(_check_ir_validity=False)
+
+    print("Exporting program...")
+    core_aten_ep = export(m, example_inputs)
+    print("Lowering to edge...")
+    edge_manager: EdgeProgramManager = to_edge(
+        core_aten_ep,
+        compile_config=edge_config,
+    )
+    print("Creating ExecuTorch program...")
+    et_program: ExecutorchProgramManager = edge_manager.to_executorch()
 
 # Write the serialized ExecuTorch program to a file.
 with open(OUTPUT_FILE, "wb") as file:
